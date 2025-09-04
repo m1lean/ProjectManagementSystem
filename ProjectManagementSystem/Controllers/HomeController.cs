0fabfe96334bdb10130385a4f5ab4985c05c3d51
Alexander Tsyhanok <altnntx@gmail.com>
2025-09-04 20:36:05 +0300

    6

---
+++ ProjectManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
